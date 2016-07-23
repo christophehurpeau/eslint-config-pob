7f4579b215e74412733ec5b8c3c7e02c37a89aab
Christophe Hurpeau <christophe@hurpeau.com>
2016-07-23 02:29:58 +0200

    temp fix https://github.com/babel/babel-eslint/issues/316

--- babel.js
+++ babel.js
@@ -4,5 +4,9 @@ module.exports = {
         'eslint-config-airbnb-base/rules/strict',
         './rules/style',
     ].map(require.resolve),
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    rules: {
+        // https://github.com/babel/babel-eslint/issues/316
+        'generator-star-spacing': 'off',
+    }
 };
