261c54729b6789b0b647ac3ceed7833d74bde40f
Christophe Hurpeau <christophe@hurpeau.com>
2016-08-18 20:41:05 +0200

    jsx 2 spaces indent

--- rules/react.js
+++ rules/react.js
@@ -5,10 +5,6 @@ module.exports = {
     // 'react/no-deprecated': 2,
     'react/no-direct-mutation-state': 2,
 
-    // changed rules
-    'react/jsx-indent-props': [2, 4],
-    'react/jsx-indent': [2, 4],
-
     // babel plugin react-require
     'react/react-in-jsx-scope': 0,
 
