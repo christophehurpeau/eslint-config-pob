5bee0e073449e60426fa337a42f3fb12d0cbe031
Christophe Hurpeau <christophe@hurpeau.com>
2016-09-24 13:48:25 +0200

    flow: remove valid-syntax (deprecated), add no-dupe-keys, boolean-style, semi, space-before-generic-bracket, union-intersection-spacing

--- rules/flow.js
+++ rules/flow.js
@@ -1,16 +1,19 @@
 module.exports = {
   plugins: ['flowtype'],
   rules: {
+    'flowtype/boolean-style': ['error', 'boolean'],
+    'flowtype/no-dupe-keys': 'error',
     'flowtype/require-parameter-type': ['warn', { excludeArrowFunctions: 'expressionsOnly' }],
     'flowtype/require-return-type': ['warn', 'always', {
       excludeArrowFunctions: true,
       annotateUndefined: 'never',
     }],
-
-    'flowtype/space-before-type-colon': ['error'],
-    'flowtype/space-after-type-colon': ['error'],
-    'flowtype/type-id-match': ['error'],
-    'flowtype/valid-syntax': ['error'],
+    'flowtype/semi': 'error',
+    'flowtype/space-after-type-colon': 'error',
+    'flowtype/space-before-generic-bracket': 'error',
+    'flowtype/space-before-type-colon': 'error',
+    'flowtype/type-id-match': 'error',
+    'flowtype/union-intersection-spacing': 'error',
     'flowtype/use-flow-type': ['error', { 'no-unused-vars': 1 }],
   },
 };
