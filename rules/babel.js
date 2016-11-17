52d89883c4e9d37e467f12ff105c098834490af8
Christophe Hurpeau <christophe@hurpeau.com>
2016-11-18 00:12:26 +0100

    eslint-plugin-babel@4

--- rules/babel.js
+++ rules/babel.js
@@ -1,19 +1,14 @@
 module.exports = {
   plugins: ['babel'],
   rules: {
-    // https://github.com/babel/babel-eslint/issues/316
-    'generator-star-spacing': 0,
-    'babel/generator-star-spacing': [2, { before: false, after: true }],
     // 'new-cap': 0,
     // 'babel/new-cap':  [2, { newIsCap: true }],
-    // 'array-bracket-spacing': 0,
-    // 'babel/array-bracket-spacing': [2, 'never'],
     // 'object-curly-spacing': 0,
     // 'babel/object-curly-spacing': [2, 'always'],
     'object-shorthand': 0,
     'babel/object-shorthand': [2, 'methods'],
 
-    'babel/func-params-comma-dangle': [2, 'always-multiline'],
+    'comma-dangle': ['error', 'always-multiline'],
 
     // disable arrow-parens because it breaks with flow
     'arrow-parens': 'off',
