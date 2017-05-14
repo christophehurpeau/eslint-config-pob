6954ca98db0aede4d9deab7255bb897f4040b24a
Christophe Hurpeau <christophe@hurpeau.com>
2017-05-14 11:14:56 +0200

    feat: enable arrow-parens https://github.com/eslint/eslint/pull/7436
    BREAKING CHANGE: enabled rule arrow-parens

--- rules/babel.js
+++ rules/babel.js
@@ -6,9 +6,6 @@ module.exports = {
     // 'object-curly-spacing': 0,
     // 'babel/object-curly-spacing': [2, 'always'],
 
-    // disable arrow-parens because it breaks with flow
-    'arrow-parens': 'off',
-
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default.md
     // `export default from './foo'` should work
     'import/no-named-as-default': 'off',
