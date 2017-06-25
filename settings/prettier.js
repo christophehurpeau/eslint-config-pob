dd4dea7c84c1ae00c160a8dfaeda3cd1648c9caf
Christophe Hurpeau <christophe@hurpeau.com>
2017-06-26 01:39:30 +0200

    fix: prettier printWidth

--- settings/prettier.js
+++ settings/prettier.js
@@ -3,7 +3,7 @@ module.exports = {
   extends: ['prettier'],
 
   rules: {
-    'prettier/prettier': ['error', { trailingComma: 'all', singleQuote: true }],
+    'prettier/prettier': ['error', { trailingComma: 'all', singleQuote: true, printWidth: 100 }],
     // disabled in prettier plugin, but compatible with prettier when 'multi-line'
     curly: ['error', 'multi-line'],
   },
