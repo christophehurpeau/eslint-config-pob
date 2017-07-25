433e1c991c2c82c8958bb19f3c13632d933937a0
Christophe Hurpeau <christophe@hurpeau.com>
2017-07-25 18:41:35 +0200

    refactor: remove same rules than airbnb

--- rules/style.js
+++ rules/style.js
@@ -6,7 +6,6 @@ module.exports = {
     // changed rules
     'no-unused-vars': [2, { args: 'none' }],
     'object-shorthand': [2, 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
 
     // disabled rules
     'no-else-return': 'off', // http://eslint.org/docs/rules/no-else-return
