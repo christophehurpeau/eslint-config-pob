ddfe57664785feea1ab4f8b204a8452958956572
Christophe Hurpeau <christophe@hurpeau.com>
2017-08-16 23:49:58 +0200

    feat: replace config es5 by node-lts
    BREAKING CHANGE: removed config es5

--- node-lts.js
+++ node-lts.js
@@ -1,10 +1,12 @@
 module.exports = {
   extends: [
-    'eslint-config-airbnb-base',
+    'eslint-config-airbnb-base/legacy',
     './settings/globals',
     './settings/prettier-es5',
+    './settings/node-lts',
     './rules/code-quality',
     './rules/style',
     './rules/expert',
+    './rules/node-lts',
   ].map(require.resolve),
 };
---
+++ settings/node-lts.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: 'script',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: false,
+    },
+  },
+  env: {
+    es6: true,
+  },
+};
