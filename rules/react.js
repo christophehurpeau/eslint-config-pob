6387010d87694d3a187451e08a3943e5482df6e8
Christophe Hurpeau <christophe@hurpeau.com>
2018-03-09 11:02:46 +0100

    feat: react enable react/jsx-handler-names
    BREAKING CHANGE: enabled react/jsx-handler-names

--- rules/react.js
+++ rules/react.js
@@ -1,18 +1,23 @@
 module.exports = {
   rules: {
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-duplicate-props.md
-    // don't ignore case
-    'react/jsx-no-duplicate-props': 'error',
+    /* added rules */
+
+    // Enforce event handler naming conventions in JSX
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-handler-names.md
+    'react/jsx-handler-names': ['error', {
+      eventHandlerPrefix: 'handle',
+      eventHandlerPropPrefix: 'on',
+    }],
 
     // Prevent direct mutation of this.state
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
     'react/no-direct-mutation-state': 'error',
 
-    // babel plugin react-require
-    'react/react-in-jsx-scope': 'off',
+    /* changed rules */
 
-    // https://github.com/yannickcr/eslint-plugin-react/issues/1009
-    'react/require-default-props': 'off',
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-duplicate-props.md
+    // don't ignore case
+    'react/jsx-no-duplicate-props': 'error',
 
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/sort-comp.md
     'react/sort-comp': [
@@ -61,5 +66,13 @@ module.exports = {
         },
       },
     ],
+
+    /* disabled rules */
+
+    // babel plugin react-require
+    'react/react-in-jsx-scope': 'off',
+
+    // https://github.com/yannickcr/eslint-plugin-react/issues/1009
+    'react/require-default-props': 'off',
   },
 };
