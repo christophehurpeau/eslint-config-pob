85424f80fefd44c544eda099076f28fba6eacc51
Christophe Hurpeau <christophe@hurpeau.com>
2018-03-09 11:00:07 +0100

    refactor: jest config remove import/no-extraneous-dependencies override, now supported in airbnb config

--- settings/jest.js
+++ settings/jest.js
@@ -1,11 +1,8 @@
 module.exports = {
   overrides: [
     {
-      files: ['src/**/__tests__/*.js', 'src/**/*test.js'],
+      files: ['src/**/__tests__/*.js', 'src/**/*.test.js'],
       env: { jest: true },
-      rules: {
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-      },
     },
   ],
 };
