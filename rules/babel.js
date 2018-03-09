33ec3c23670076fcf86823d649bb4cc430b7f28a
Christophe Hurpeau <christophe@hurpeau.com>
2018-03-09 11:02:39 +0100

    feat: enable import/no-commonjs when using babel
    BREAKING CHANGE: import/no-commonjs enabled

--- rules/babel.js
+++ rules/babel.js
@@ -5,5 +5,9 @@ module.exports = {
     // 'babel/new-cap':  [2, { newIsCap: true }],
     // 'object-curly-spacing': 0,
     // 'babel/object-curly-spacing': [2, 'always'],
+
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-commonjs.md
+    // disallow require when using babel
+    'import/no-commonjs': 'error',
   },
 };
