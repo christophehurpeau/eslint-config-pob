ab846774e034f59dc95fafcb753652319c8c6801
Christophe Hurpeau <christophe@hurpeau.com>
2018-11-23 17:13:25 +0100

    fix(typescript): disable no-use-before-define

--- typescript.js
+++ typescript.js
@@ -20,24 +20,28 @@ module.exports = {
   rules: {
     'import/extensions': ['error', { extensions: ['ts', 'js'] }],
 
-    // typescript plugin
-    'typescript/adjacent-overload-signatures': 'error',
-    'typescript/member-ordering': 'error',
-    'typescript/no-angle-bracket-type-assertion': 'error',
-    'typescript/no-array-constructor': 'error',
-
-    // interface can be used for empty props
-    'typescript/no-empty-interface': 'off',
-    'typescript/no-namespace': 'error',
-    'typescript/no-unused-vars': 'error',
-    'typescript/no-use-before-define': 'error',
-    'typescript/no-var-requires': 'error',
-
-    // issues
+    /* issues */
     'no-undef': 'off',
     // https://github.com/eslint/typescript-eslint-parser/issues/414
     'no-restricted-globals': 'off',
     // issue with generics
     'no-unused-vars': 'off',
+    // issue when reexporting types
+    'no-use-before-define': 'off',
+
+    /* typescript plugin */
+    'typescript/adjacent-overload-signatures': 'error',
+    'typescript/member-ordering': 'error',
+    'typescript/no-angle-bracket-type-assertion': 'error',
+    'typescript/no-array-constructor': 'error',
+    'typescript/no-use-before-define': 'error',
+    'typescript/no-var-requires': 'error',
+
+    // interface can be used for empty props
+    'typescript/no-empty-interface': 'off',
+
+    /* doesn't work without base rule */
+    'typescript/no-namespace': 'off',
+    'typescript/no-unused-vars': 'off',
   },
 };
