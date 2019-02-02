a08fa29c2844d5c8ec3db2f7e8b6671f952dce33
Christophe Hurpeau <christophe@hurpeau.com>
2019-02-02 18:56:42 +0100

    feat: use @typescript-eslint
    BREAKING CHANGE: eslint-plugin-typescript replaced by @typescript-eslint

--- typescript.js
+++ typescript.js
@@ -1,8 +1,14 @@
 'use strict';
 
 module.exports = {
-  parser: 'typescript-eslint-parser',
-  plugins: ['typescript'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@​typescript-eslint'],
+
+  extends: [
+    // disable base eslint rules, plugin rules are overrided below
+    'plugin:@typescript-eslint/recommended',
+    require.resove('eslint-config-prettier/@typescript-eslint'),
+  ],
 
   settings: {
     'import/resolver': {
@@ -20,28 +26,58 @@ module.exports = {
   rules: {
     'import/extensions': ['error', { extensions: ['ts', 'js'] }],
 
-    /* issues */
-    'no-undef': 'off',
-    // https://github.com/eslint/typescript-eslint-parser/issues/414
-    'no-restricted-globals': 'off',
-    // issue with generics
-    'no-unused-vars': 'off',
-    // issue when reexporting types
-    'no-use-before-define': 'off',
+    /* typescript-eslint rules */
+    // disabled by prettier : indent, member-delimiter-style, type-annotation-spacing
 
-    /* typescript plugin */
-    'typescript/adjacent-overload-signatures': 'error',
-    'typescript/member-ordering': 'error',
-    'typescript/no-angle-bracket-type-assertion': 'error',
-    'typescript/no-array-constructor': 'error',
-    'typescript/no-use-before-define': 'error',
-    'typescript/no-var-requires': 'error',
+    /* same as recommended */
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/ban-types': 'error',
+    '@typescript-eslint/camelcase': 'error',
+    '@typescript-eslint/class-name-casing': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/interface-name-prefix': 'error',
+    '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
+    '@typescript-eslint/no-array-constructor': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-object-literal-type-assertion': 'error',
+    '@typescript-eslint/no-parameter-properties': 'error',
+    '@typescript-eslint/no-triple-slash-reference': 'error',
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/prefer-interface': 'error',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+
+    '@typescript-eslint/generic-type-naming': 'off',
+    '@typescript-eslint/member-naming': 'off',
+    '@typescript-eslint/member-ordering': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/no-type-alias': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+
+    /* enabled */
+    '@typescript-eslint/no-useless-constructor': 'error',
+
+    /* changed */
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none',
+        ignoreRestSiblings: true,
+      },
+    ],
+
+    /* disabled */
 
     // interface can be used for empty props
-    'typescript/no-empty-interface': 'off',
-
-    /* doesn't work without base rule */
-    'typescript/no-namespace': 'off',
-    'typescript/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
   },
 };
