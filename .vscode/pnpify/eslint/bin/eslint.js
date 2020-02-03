59c3d6d411ff3ae42de005552f4f2c2e15f11ca3
Christophe Hurpeau <christophe@hurpeau.com>
2020-02-03 23:27:36 +0100

    chore: fix pnpify

--- .vscode/pnpify/eslint/bin/eslint.js
+++ .vscode/pnpify/eslint/bin/eslint.js
@@ -3,13 +3,13 @@
 const {createRequire, createRequireFromPath} = require(`module`);
 const {resolve} = require(`path`);
 
-const relPnpApiPath = "../../../.pnp.js";
+const relPnpApiPath = "../../../../.pnp.js";
 
 const absPnpApiPath = resolve(__dirname, relPnpApiPath);
 const absRequire = (createRequire || createRequireFromPath)(absPnpApiPath);
 
-// Setup the environment to be able to require prettier/index.js
+// Setup the environment to be able to require eslint/bin/eslint.js
 require(absPnpApiPath).setup();
 
-// Defer to the real prettier/index.js your application uses
-module.exports = absRequire(`prettier/index.js`);
+// Defer to the real eslint/bin/eslint.js your application uses
+module.exports = absRequire(`eslint/bin/eslint.js`);
