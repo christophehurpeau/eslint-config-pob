f5de547842096b53f525f784f01ad183e0dc10e1
Christophe Hurpeau <christophe@hurpeau.com>
2020-12-13 15:45:25 +0100

    fix: import in typescript-react

--- @pob/eslint-config-typescript-react/lib/index.js
+++ @pob/eslint-config-typescript-react/lib/index.js
@@ -5,7 +5,7 @@ module.exports = {
     'eslint-config-airbnb/rules/react',
     'eslint-config-airbnb/rules/react-a11y',
     'eslint-config-airbnb/rules/react-hooks',
-    '@pob/eslint-config/prettier', // reapply prettier
+    '@pob/eslint-config/lib/prettier', // reapply prettier
     'eslint-config-prettier/react',
     '@pob/eslint-config/lib/rules/expert', // reapply expert: eslint-config-airbnb/rules/react modifies no-underscore-dangle
     './rules/react',
