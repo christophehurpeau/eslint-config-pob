09d546ca8542e04bc309699f7945f3307647e3d4
Christophe Hurpeau <christophe@hurpeau.com>
2021-01-09 13:12:24 +0100

    feat: update eslint-config-prettier

--- @pob/eslint-config/lib/prettier.js
+++ @pob/eslint-config/lib/prettier.js
@@ -4,16 +4,6 @@ module.exports = {
   extends: ['eslint-config-prettier'].map(require.resolve),
 
   rules: {
-    // https://github.com/prettier/eslint-config-prettier#arrow-body-style-and-prefer-arrow-callback
-    // issue: https://github.com/prettier/eslint-plugin-prettier/issues/65
-    'prefer-arrow-callback': [
-      'error',
-      {
-        allowNamedFunctions: false,
-        allowUnboundThis: true,
-      },
-    ],
-
     // https://github.com/prettier/eslint-config-prettier#curly
     // prettier doesn't enforce {} with multiline
     curly: ['error', 'multi-line'],
