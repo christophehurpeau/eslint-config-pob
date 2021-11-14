12cdd732b21611ab24259f13137b8628f3b676fb
Christophe Hurpeau <christophe@hurpeau.com>
2021-11-14 16:38:40 +0100

    feat: enable all typescript-eslint rules by default
    BREAKING CHANGE: typescript rules added

--- @pob/eslint-config-typescript/lib/plugins/typescript-eslint.js
+++ @pob/eslint-config-typescript/lib/plugins/typescript-eslint.js
@@ -6,47 +6,19 @@ usePlugin(require('@typescript-eslint/eslint-plugin/package.json'));
 
 module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
+  extends: ['plugin:@typescript-eslint/all'],
 
   rules: {
     // disabled by prettier : indent, member-delimiter-style, type-annotation-spacing
 
     /* same as recommended */
-    '@typescript-eslint/adjacent-overload-signatures': 'error',
-    '@typescript-eslint/ban-ts-comment': 'error',
-    '@typescript-eslint/ban-types': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'warn',
-    '@typescript-eslint/no-array-constructor': 'error',
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-extra-non-null-assertion': 'error',
-    '@typescript-eslint/no-extra-semi': 'error',
-    '@typescript-eslint/no-inferrable-types': 'error',
-    '@typescript-eslint/no-misused-new': 'error',
-    '@typescript-eslint/no-namespace': 'error',
-    '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
     '@typescript-eslint/no-non-null-assertion': 'warn',
-    '@typescript-eslint/no-this-alias': 'error',
-    '@typescript-eslint/no-var-requires': 'error',
-    '@typescript-eslint/prefer-namespace-keyword': 'error',
-    '@typescript-eslint/triple-slash-reference': 'error',
 
-    /* same as recommended-requiring-type-checking */
-    '@typescript-eslint/await-thenable': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-for-in-array': 'error',
-    '@typescript-eslint/no-implied-eval': 'error',
-    '@typescript-eslint/no-misused-promises': 'error',
-    '@typescript-eslint/no-unsafe-assignment': 'error',
-    '@typescript-eslint/no-unsafe-call': 'error',
-    '@typescript-eslint/no-unsafe-member-access': 'error',
-    '@typescript-eslint/no-unsafe-return': 'error',
-    '@typescript-eslint/prefer-regexp-exec': 'error',
-    '@typescript-eslint/require-await': 'error',
-    '@typescript-eslint/restrict-plus-operands': 'error',
-    '@typescript-eslint/restrict-template-expressions': 'error',
+    /* disabled */
+
+    // too much errors on existing code
+    '@typescript-eslint/unbound-method': 'off',
 
     /* Not enabled */
 
@@ -70,41 +42,9 @@ module.exports = {
     '@typescript-eslint/keyword-spacing': 'off',
     '@typescript-eslint/lines-between-class-members': 'off',
 
-    /* enabled */
-    '@typescript-eslint/array-type': 'error',
-    '@typescript-eslint/ban-tslint-comment': 'error',
-    '@typescript-eslint/no-parameter-properties': 'error',
-    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-    '@typescript-eslint/no-useless-constructor': 'error',
-    '@typescript-eslint/prefer-includes': 'error',
-    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
-    '@typescript-eslint/prefer-function-type': 'error',
-    '@typescript-eslint/prefer-readonly': 'error',
-    '@typescript-eslint/prefer-optional-chain': 'error',
-    '@typescript-eslint/method-signature-style': 'error',
-    '@typescript-eslint/prefer-reduce-type-parameter': 'error',
-    '@typescript-eslint/prefer-ts-expect-error': 'error',
-    '@typescript-eslint/no-invalid-void-type': 'error',
-    '@typescript-eslint/no-confusing-non-null-assertion': 'error',
-    '@typescript-eslint/prefer-literal-enum-member': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error',
-    'no-redeclare': 'off',
-    '@typescript-eslint/no-redeclare': 'error',
-    '@typescript-eslint/consistent-indexed-object-style': 'error',
-    '@typescript-eslint/no-unnecessary-type-constraint': 'error',
-    '@typescript-eslint/non-nullable-type-assertion-style': 'error',
-
     // enable dot-notation, like https://github.com/airbnb/javascript/blob/5b462a04cdfebba712a3df18926edfcb4103d2a9/packages/eslint-config-airbnb-base/rules/best-practices.js#L40-L41
     'dot-notation': 'off',
     '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
-
-    // enable no-loss-of-precision
-    'no-loss-of-precision': 'off',
-    '@typescript-eslint/no-loss-of-precision': ['error'],
-
-    // fix no-use-before-define
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
 
     // Testing rules as warn
     '@typescript-eslint/prefer-as-const': 'warn',
@@ -147,6 +87,11 @@ module.exports = {
     '@typescript-eslint/no-confusing-void-expression': 'warn',
 
     /* changed */
+
+    // prefer interface
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+
+    // allow expressions
     '@typescript-eslint/explicit-function-return-type': [
       'error',
       {
@@ -171,18 +116,5 @@ module.exports = {
         ignoreRestSiblings: true,
       },
     ],
-
-    /* disabled */
-
-    // some callback/implmentations could do nothing
-    '@typescript-eslint/no-empty-function': 'off',
-
-    // interface can be used for empty props
-    '@typescript-eslint/no-empty-interface': 'off',
-    // too much errors on existing code
-    '@typescript-eslint/unbound-method': 'off',
-
-    // issue when as is used to remove undefined (in liwi for example)
-    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
   },
 };
