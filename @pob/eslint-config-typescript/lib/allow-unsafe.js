bd6c23aa12a16048187cc9d4a59923858915c3c3
Christophe Hurpeau <christophe@hurpeau.com>
2022-01-30 15:56:11 +0100

    feat: add allow-unsafe

---
+++ @pob/eslint-config-typescript/lib/allow-unsafe.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+  },
+};
