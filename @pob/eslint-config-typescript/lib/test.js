9b01525de5630a57b7dc429e6a28afa5825fdf02
Christophe Hurpeau <christophe@hurpeau.com>
2022-07-07 21:43:17 +0200

    fix: disable @typescript-eslint/no-unsafe-return in tests

--- @pob/eslint-config-typescript/lib/test.js
+++ @pob/eslint-config-typescript/lib/test.js
@@ -5,6 +5,9 @@ module.exports = {
     // enable (as any).something in test
     '@typescript-eslint/no-unsafe-member-access': 'off',
 
+    // jest.mock in test
+    '@typescript-eslint/no-unsafe-return': 'off',
+
     // don't force return type in test
     '@typescript-eslint/explicit-function-return-type': 'off',
   },
