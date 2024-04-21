0094461a633be153a734a4f52cfaa548f92a6e1e
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-04-21 13:36:09 +0200

    fix: properly configure module vs commonjs

---
+++ @pob/eslint-config/lib/base/commonjs.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  parserOptions: {
+    sourceType: 'script',
+  },
+  rules: {
+    strict: ['error', 'safe'],
+  },
+};
--- @pob/eslint-config/lib/node-module.js
+++ @pob/eslint-config/lib/node-module.js
@@ -1,7 +1,25 @@
 'use strict';
 
 module.exports = {
-  extends: ['./base', './plugins/import/import-module', './node/module'].map(
-    require.resolve,
-  ),
+  extends: [
+    './_base',
+    './base/module',
+    './plugins/import/import-module',
+    './node/module',
+  ].map(require.resolve),
+
+  overrides: [
+    {
+      files: ['*.cjs'],
+      extends: [
+        './_base',
+        './base/commonjs',
+        './plugins/import/import-commonjs',
+        './node/commonjs',
+      ].map(require.resolve),
+    },
+    {
+      files: ['*.mjs'],
+    },
+  ],
 };
