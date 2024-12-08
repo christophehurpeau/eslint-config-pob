4b056c6911447fe9029d5d7cfba1264508bb6f0b
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-12-08 16:53:44 +0100

    feat: add moduleDirectory in app config

--- @pob/eslint-config-typescript/lib/overrides/app.js
+++ @pob/eslint-config-typescript/lib/overrides/app.js
@@ -1,5 +1,8 @@
 export default {
   name: "@pob/eslint-config-typescript/app",
+  settings: {
+    node: { moduleDirectory: ["node_modules", "src"] },
+  },
   rules: {
     /* No need for this rule in an app */
     "@typescript-eslint/explicit-module-boundary-types": "off",
