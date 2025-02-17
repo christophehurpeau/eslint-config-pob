4b2db24350cd5a3ce31a65dc37fd5d01bf3f666e
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-02-17 23:49:44 +0100

    feat: enable reportUnusedInlineConfigs

--- @pob/eslint-config/lib/_base.js
+++ @pob/eslint-config/lib/_base.js
@@ -16,6 +16,7 @@ export default [
     linterOptions: {
       // todo noInlineConfig: true,
       reportUnusedDisableDirectives: "error",
+      reportUnusedInlineConfigs: "error",
     },
   },
   {
