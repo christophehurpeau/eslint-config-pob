7c4334daf3b5fe19ed058e4d0861c0d6a088f340
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-06-20 16:54:09 +0200

    feat: increase max complexity

--- @pob/eslint-config/lib/rules/code-quality.js
+++ @pob/eslint-config/lib/rules/code-quality.js
@@ -1,7 +1,7 @@
 export default {
   name: "@pob/eslint-config/code-quality",
   rules: {
-    complexity: ["warn", { max: 8 }],
+    complexity: ["warn", { max: 20 }],
     "max-depth": ["warn", 6],
   },
 };
