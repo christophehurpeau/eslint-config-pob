d7553b54797cc4badccaf232241a280d114a0051
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-12-26 14:00:40 +0100

    fix: remove import-x/resolver-next override for apps

--- @pob/eslint-config-typescript/lib/overrides/app.js
+++ @pob/eslint-config-typescript/lib/overrides/app.js
@@ -1,15 +1,5 @@
-import { createNodeResolver } from "eslint-plugin-import-x";
-
 export default {
   name: "@pob/eslint-config-typescript/app",
-  settings: {
-    "import-x/resolver-next": [
-      createNodeResolver({
-        // @ts-expect-error missing types
-        moduleDirectory: ["node_modules", "src"],
-      }),
-    ],
-  },
   rules: {
     /* No need for this rule in an app */
     "@typescript-eslint/explicit-module-boundary-types": "off",
